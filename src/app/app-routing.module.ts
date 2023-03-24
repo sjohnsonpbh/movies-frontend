85b0bc889c85c1e6e30bb78cc8570f97e1b86b97
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-03-24 08:03:26 -0500

    Added auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { AuthComponent } from './auth/auth.component';
+
 import { MoviesComponent } from './movies/movies.component';
 import { ReviewRateComponent } from './review-rate/review-rate.component';
-import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
+  { path: 'auth', component: AuthComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'review', component: ReviewRateComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 import { ReviewRateComponent } from './review-rate/review-rate.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
-  declarations: [AppComponent, MoviesComponent, LoginComponent, SignupComponent, ReviewRateComponent],
+  declarations: [
+    AppComponent,
+    MoviesComponent,
+    ReviewRateComponent,
+    AuthComponent,
+  ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
